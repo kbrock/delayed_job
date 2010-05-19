572712a1d9807ed86c3c5667b0c03ca8a14a6ae8
Keenan Brock <keenan@thebrocks.net>
2010-05-19 19:04:48 -0400

    added tasks to clear out successful and failed jobs

--- lib/delayed/tasks.rb
+++ lib/delayed/tasks.rb
@@ -3,13 +3,24 @@ task :environment
 task :merb_env
 
 namespace :jobs do
-  desc "Clear the delayed_job queue."
+  desc "Alias for :clear:all"
   task :clear => :"clear:all"
+
   namespace :clear do
 
     desc "Clear the whole delayed_job queue."
     task :all => [:merb_env, :environment] do
       Delayed::Job.delete_all
+    end
+
+    desc "Clear finished jobs."
+    task :finished => [:merb_env, :environment] do
+      Delayed::Job.delete_all('finished_at IS NOT NULL')
+    end
+
+    desc "Clear failed jobs."
+    task :failed => [:merb_env, :environment] do
+      Delayed::Job.delete_all('failed_at IS NOT NULL')
     end
   end
 
